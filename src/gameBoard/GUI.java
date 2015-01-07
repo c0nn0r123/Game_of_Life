920ecf7bf8c6f17a754a7faf6bfdc15251221daa
Connor <conyk263@yahoo.com>
2015-01-06 19:09:17 -0500

    movement and still life starter method

--- src/gameBoard/GUI.java
+++ src/gameBoard/GUI.java
@@ -13,44 +13,25 @@ public class GUI implements ActionListener {
 	JPanel mainPanel = new JPanel(new GridLayout(100,100,2,2));
 	JPanel[][] lifePanel = new JPanel[100][100];
 	boolean[][] alive = new boolean[100][100];
-<<<<<<< HEAD
-	Timer timer = new Timer(1000, this);
 	Scanner rd = new Scanner(System.in);
-=======
-	Timer timer = new Timer(2000, this);
->>>>>>> origin/master
+	Timer timer = new Timer(500, this);
+
 	public GUI(){
 		
 		System.out.println("what Shape?");
-		System.out.println("Boat, Ship, Beehive, or loaf");
+		System.out.println("Boat, Ship, Beehive, loaf, or move");
 		String Shape = rd.nextLine();
 		
 		//creating first alive and dead panels
 		for(int i = 0; i < 100; i++){
 			for(int x = 0; x < 100; x++){
 				lifePanel[i][x] = new JPanel();
-				if(i > 30 && i < 50 && x > 47 && x < 50){
-					lifePanel[i][x].setBackground(Color.white);
-<<<<<<< HEAD
-					alive[i][x] = true;	
-=======
-<<<<<<< HEAD
-					alive[i][x] = true;	
-=======
-					
-					alive[i][x] = true;
-					alive[i][x]=true;	
-
-					alive[i][x] = true;
-
-					alive[i][x]=true;	
-
->>>>>>> origin/master
->>>>>>> origin/master
-				}else{
+				alive[i][x] = false;
+				startShape(i,x,Shape);
+				if(!alive[i][x])
 					lifePanel[i][x].setBackground(Color.black);
-					alive[i][x] = false;
-				}
+				else
+					lifePanel[i][x].setBackground(Color.white);
 				mainPanel.add(lifePanel[i][x]);
 				}
 			}
@@ -63,7 +44,32 @@ public class GUI implements ActionListener {
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		timer.start();
 	}
-	public boolean deadOrAlive(boolean cell,int x,int y){
+	///////still life starters and the movement starter////////////
+	public void startShape(int y, int x, String state){
+		if (state.equalsIgnoreCase("beehive")){
+			if (((y == 45 || y == 46) && (x == 43 || x == 45)) || ((x == 44) && (y == 44 || y == 47))){
+				alive[y][x] = true;
+			}
+		}else if(state.equalsIgnoreCase("boat")){
+			if((y == 45 && (x == 44 || x == 46)) || (y == 46 && ( x == 45 || x == 46)) || (y == 44 && x == 45)){
+				alive[y][x] = true;
+			}
+		}else if(state.equalsIgnoreCase("ship")){
+			if((y == 45 && (x == 44 || x == 46)) || (y == 46 && ( x == 45 || x == 46)) || (y == 44 && (x == 44 ||x == 45))){
+				alive[y][x] = true;
+			}
+		}else if(state.equalsIgnoreCase("loaf")){
+			if((y == 45 && (x == 44 || x == 46)) || (y == 46 && ( x == 44 || x == 47)) || (y == 44 && x == 45)|| (y == 47 &&(x == 45 || x == 46))){
+				alive[y][x] = true;
+			}
+		}else if(state.equalsIgnoreCase("move")){
+			if(y > 30 && y < 50 && x > 47 && x < 50){
+				alive[y][x] = true;	
+			}
+		}
+	}
+	////////////////////////////////////////////
+ 	public boolean deadOrAlive(boolean cell,int x,int y){
 
 		//a dead cell with 3 live neighbors becomes alive
 		//a live cell with 2 or 3 live neighbors survives
@@ -75,7 +81,7 @@ public class GUI implements ActionListener {
 				for(int j = -1; j<=1;j++){
 					if(((x + i >= 0) && (y + j >=0)) && ((x + i <= 99) &&  (y + j <= 99)) && !(i == 0 && j == 0)){// && i == j)){
 						if(alive[x+i][y+j]){
-							System.out.println("alive");
+							//System.out.println("alive");
 							neighbors++;
 						}
 					}
@@ -90,7 +96,7 @@ public class GUI implements ActionListener {
 				for(int j = -1; j<=1;j++){
 					if(((x + i >= 0) &&  (y + j >=0)) && ((x + i <= 99) &&  (y + j <= 99)) && !(i == 0 && j == 0 && i == j)){
 						if(alive[x+i][y+j]){
-							System.out.println("alive");
+							//System.out.println("alive");
 							neighbors++;
 						}
 					}
@@ -104,41 +110,22 @@ public class GUI implements ActionListener {
 
 		return false;
 	}
-	/*public boolean onBoard (int x,int y, int i, int j){
-		if(x+i>=0){
-	}
-		return false;
-
-	}*/
+	
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent e) {
 		for(int i = 0; i < 100; i++){
 			for(int x = 0; x < 100; x++){
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
 				alive[i][x] = deadOrAlive(alive[i][x],i,x);
 				if(alive[i][x]){
 					lifePanel[i][x].setBackground(Color.white);
 					alive[i][x]=true;
-				
 				}else{
 					lifePanel[i][x].setBackground(Color.black);
-<<<<<<< HEAD
-				mainPanel.updateUI();				
-=======
 					alive[i][x]=false;
-				}
-				mainPanel.updateUI();
-
-
-				//deadOrAlive(alive[i][x],i,x);
-
->>>>>>> origin/master
+				}	
 			}
 		}
-		
+		mainPanel.updateUI();
 		System.out.println("tick");
 	}
 }
